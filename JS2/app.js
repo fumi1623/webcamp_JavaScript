590eaaf1df1df8054376c20feaf50eaa271fe0a4
fumi1623 <ancient17.hirofumi@gmail.com>
2021-03-14 20:10:08 +0900

    [Start]

---
+++ JS2/app.js
@@ -0,0 +1,44 @@
+var hello = 'Hello World';
+// alert(hello);
+
+// var int1 = 1;
+// var int2 = -10;
+// var float1 = 3.14;
+// var str1 = 'JavaScriptを覚えよう';
+
+// alert('Hello' + 'World');
+
+// var str1 = 'Hello';
+// var str2 = 'World';
+// alert(str1 + str2);
+
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+//   alert('orange < apple');
+// } else if(orange == apple){
+//   alert('orange = apple');
+// } else{
+//   alert('orange > apple')
+// }
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足した結果は' + num + 'です')
---
+++ readme.txt
@@ -0,0 +1,6 @@
+# DMM WEBCAMPコンテンツ【JavaScriptを学ぼう】
+DMM WEBCAMPの学習コンテンツJavaScriptを学ぼうの研修課題です。
+## 使い方
+html/css/JavaScriptファイルのため、PCのブラウザで実行できます。
+
+2021/3/14
